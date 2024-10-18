71a3a1bee893a7b1dbd9861364866aff9f160a8a
pristavochka <boba0205@mail.ru>
2024-10-18 14:14:58 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+x=1
+s=1
+while x < 7:
+    s= s * x
+    x+=1
+print(s)
+
+
+
+
---
+++ задания для контрольной.py
@@ -0,0 +1,8 @@
+from random import *
+seed(1025)
+task1=randint(1,18)
+task2=randint(1,13)
+task3=randint(1,14)
+print(task1, task2, task3)
+
+
