13901a3782cb08d7244338f282622cafe8d461b5
pristavochka <boba0205@mail.ru>
2024-11-08 20:21:59 +0300

    Update задание 4 уровень 3 лаба 3.py

--- лаба 3/задание 4 уровень 3 лаба 3.py
+++ лаба 3/задание 4 уровень 3 лаба 3.py
@@ -2,15 +2,20 @@ from random import *
 n= int(input())
 B=[]
 a=[]
+r=0
 for i in range(0, n-1):
     B.append(randint(1, 50))
 print(B)
 for i in range(0, n-1):
-    a.append(B[i])
     if B[i]==max(B):
         if i==0:
             a[i]=0
+        elif i==1:
+            a[i]=a[0]
         else:
-            for x in range(1, i):
-                a[i]+=B[x-1]
+            for x in range(1, i+1):
+                r+=B[x-1]
+            a.append(r)
+    else:
+        a.append(B[i])
 print(a)
