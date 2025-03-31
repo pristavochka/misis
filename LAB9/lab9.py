e16eb024c53661280dadaa863def498129684755
pristavochka <boba0205@mail.ru>
2025-03-31 13:47:12 +0300

    Add files via upload

---
+++ LAB9/lab9.py
@@ -0,0 +1,20 @@
+f = open(r"c:\Users\m2406454\Desktop\LAB9\arrayin.txt", 'r')
+e = f.read()
+a = e.split(' ')
+c=0
+for b in range(len(a)):
+    a[b] = int(a[b])
+    c += a[b]
+for i in range(len(a)):
+    if a[i] == max(a):
+        if a[i] > c:
+            a[i]=0
+        else:
+            a[i]=2*a[i]
+f = open(r"c:\Users\m2406454\Desktop\LAB9\arrayout.txt", 'w')
+for i in a:
+    i=str(i)
+    f.write(i + ' ')
+print(a)
+
+
