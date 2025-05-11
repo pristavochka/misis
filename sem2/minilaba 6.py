b288adde9e98dd0939cfa2e0de335346913dec0f
pristavochka <boba0205@mail.ru>
2025-05-11 21:52:36 +0300

    Update minilaba 6.py

--- sem2/minilaba 6.py
+++ sem2/minilaba 6.py
@@ -1,41 +1,50 @@
-from math import e 
-x=3
+from math import sin, cos
 
-speed = 0.00001
+x = 3
+
+speed = 0.01
 
 epochs = 100
 
+
 def difffunc(x):
-    y = 2*x*e**(x**2)+2*x 
+    y = 2*x
     return y
+
+
 def func(x):
-    y = e**(x**2) + x**2
-    return y 
+    y = x**2
+    return y
+
+
 def gradiendDescend(x, speed, epochs):
-    xList=[]
-    yList=[]
+    xList = []
+    yList = []
     for i in range(1, epochs):
-        x = x - speed * difffunc(x) 
+        x = x - speed * difffunc(x)
         xList.append(x)
         yList.append(func(x))
     return xList, yList
-data = gradiendDescend(x, speed, epochs)
 
+
+data = gradiendDescend(x, speed, epochs)
 
 import matplotlib.pyplot as plt
 import numpy as np
- 
+
 fig, ax = plt.subplots()
-x = np.linespace(0,10,100)
+x = np.linspace(0, 10, 100)
 ax.plot(x, func(x))
 plt.show()
-sizes = np.random.uniform(15, 80, len(x))
-colors = np.random.uniform(15, 80, len(x))
+sizes = np.random.uniform(15, 80, len(x)-1)
+colors = np.random.uniform(15, 80, len(x)-1)
 fig, ax = plt.subplots()
 
-ax.scatter(data[0], data[1], s=sizes, c=colors, vmin=0, vmax=100)
+print(data, len(data[0]),len(data[1]))
 
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
+ax.scatter(data[0], data[1], s = sizes, c = colors, vmin=0, vmax=100)
+
+ax.set(xlim=(0, 8), xticks=np.arange(0, 10),
+       ylim=(0, 8), yticks=np.arange(0, 10))
 
 plt.show()
